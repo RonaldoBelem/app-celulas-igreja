1eea759a4b040f3bd5089278ecd3933524c05370
Igreja Ranking App <igreja-ranking@example.com>
2025-04-09 19:01:47 -0300

    first commit

---
+++ app/src/main/java/com/example/celulasapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.celulasapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webView.loadUrl("https://celulas-igreja.vercel.app/"); // Substitua pelo URL do seu site
+        webSettings.setJavaScriptEnabled(true);  // Habilita JavaScript
+        webSettings.setDomStorageEnabled(true);
+
+    }
+}
